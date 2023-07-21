01acb4fedadd2c7b8858c327645ac40ae8927d3d
Steven Correa <97488936+Aegisystem@users.noreply.github.com>
2023-07-21 02:49:52 -0500

    Lose screen and timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject loseImage;
+
+    public float time = 2f;
+    
+    public static GameManager Instance;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Instance = this;
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            loseImage.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI tmp;
+    private float time;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        tmp = GetComponent<TextMeshProUGUI>();
+        tmp.outlineWidth = 0.5f;
+        tmp.outlineColor = Color.black;
+        tmp.color = Color.green;
+        UpdateTimerText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = GameManager.Instance.time;
+        if (time > 0)
+        {
+            if(time<10f && time > 9f) tmp.color = Color.red;
+            UpdateTimerText();
+        }
+        else
+        {
+            time = 0;
+        }
+    }
+
+    private void UpdateTimerText()
+    {
+        int minutes = Mathf.FloorToInt(time / 60f);
+        int seconds = Mathf.FloorToInt(time % 60f);
+        int milliseconds = Mathf.FloorToInt((time * 100f) % 100f);
+
+        tmp.text = string.Format("{0:00}:{1:00}.{2:00}", minutes, seconds, milliseconds);
+    }
+}
